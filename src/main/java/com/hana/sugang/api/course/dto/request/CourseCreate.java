0c508d044f8f9d0d694d8e17cfd5f67239e4292e
hana0627 <hanana9506@naver.com>
2023-09-22 12:47:34 +0900

    #4 유효성 검사 추가

--- src/main/java/com/hana/sugang/api/course/dto/request/CourseCreate.java
+++ src/main/java/com/hana/sugang/api/course/dto/request/CourseCreate.java
@@ -2,20 +2,27 @@ package com.hana.sugang.api.course.dto.request;
 
 import com.hana.sugang.api.course.domain.Course;
 import com.hana.sugang.api.course.domain.constant.CourseType;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
 /**
  * Course 요청 Dto타입
  */
 public record CourseCreate(
         String code, // 강의코드
+        @NotBlank(message = "강의명을 입력해주세요.")
         String title, // 강의명
         String description, // 강의설명
-        Integer maxCount, // 최대 수강가능 인원 수 
+        @Max(value = 100,message = "수강인원은 100명을 넘을 수 없습니다.")
+        Integer maxCount, // 최대 수강가능 인원 수
         CourseType courseType, //전공, 교양 여부
+        @Min(value = 1, message = "학점은 1 보다 낮을 수 없습니다.")
+        @Max(value = 3, message = "학점은 3보다 높을 수 없습니다.")
         Integer score
 
 ) {
-
 
     public static CourseCreate of(String code, String title, String description, Integer maxCount, CourseType courseType, Integer score) {
         return new CourseCreate(code, title, description, maxCount, courseType, score);
