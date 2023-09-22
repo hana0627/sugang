a62385bfa8fbf663f2d9bb07edaf36f77e19fb63
hana0627 <hanana9506@naver.com>
2023-09-22 15:16:56 +0900

    #12 멤버 엔티티변경 및 가장 기본적인 패키지 구조 정의

---
+++ src/main/java/com/hana/sugang/api/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.hana.sugang.api.member.repository;
+
+import com.hana.sugang.api.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
