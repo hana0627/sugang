388ea2d9c1e05c005ba3c2b50366db55503c58e6
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-10-14 18:33:59 +0900

    #36 의존성주입코드 변경
    @RequiredArgsConstructor 대신
    인터페이스를 필드로 선언
    구현체를 생성자에서 주입하여 컨트롤러가 구현체인 ServiceImpl이 아닌 Service를 의존하게 만들었다.
    
    이상태로 개발이 더 이루어졌다면, 나중에 변경점이 훨씬 많이 잡혔을 것 같다.
    @RequiredArgsConstructor 역시 조금 신중하게 사용할 필요가 있어보인다.

--- src/main/java/com/hana/sugang/api/course/controller/CourseController.java
+++ src/main/java/com/hana/sugang/api/course/controller/CourseController.java
@@ -5,8 +5,8 @@ import com.hana.sugang.api.course.dto.request.CourseCreate;
 import com.hana.sugang.api.course.dto.request.CourseEdit;
 import com.hana.sugang.api.course.dto.request.CourseSearch;
 import com.hana.sugang.api.course.dto.response.CourseResponse;
+import com.hana.sugang.api.course.service.CourseService;
 import com.hana.sugang.api.course.service.Impl.CourseRedisService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,25 +14,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RequiredArgsConstructor
 @RestController
 public class CourseController {
 
 //    private final CourseRDBService courseRDBService;
-    private final CourseRedisService courseRedisService;
+    private final CourseService courseService;
+
+    public CourseController(CourseRedisService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping("/courses")
     public List<CourseResponse> courseList(CourseSearch courseSearch) {
-        return courseRedisService.findCourses(courseSearch);
+        return courseService.findCourses(courseSearch);
     }
     @GetMapping("/course/{id}")
     public CourseResponse course(@PathVariable(name = "id") Long id) {
-        return courseRedisService.findOne(id);
+        return courseService.findOne(id);
     }
     @PostMapping("/course")
     public Map<String,Long> createCourse(@RequestBody @Validated CourseCreate requestDto) {
         Map<String, Long> map = new HashMap<>();
-        Long id = courseRedisService.saveCourse(requestDto);
+        Long id = courseService.saveCourse(requestDto);
 
         map.put("id",id);
         return map;
@@ -40,7 +43,7 @@ public class CourseController {
     @PatchMapping("/course/{id}")
     public Map<String, Long> editCourse(@PathVariable(name = "id") Long id, @RequestBody @Validated CourseEdit requestDto) {
         Map<String, Long> map = new HashMap<>();
-        Long courseId = courseRedisService.editCourse(id, requestDto);
+        Long courseId = courseService.editCourse(id, requestDto);
 
         map.put("id",courseId);
         return map;
@@ -48,7 +51,7 @@ public class CourseController {
     @DeleteMapping("/course/{id}")
     public Map<String, Long> deleteCourse(@PathVariable(name = "id") Long id) {
         Map<String, Long> map = new HashMap<>();
-        courseRedisService.deleteCourse(id);
+        courseService.deleteCourse(id);
 
         map.put("id",id);
         return map;
@@ -62,7 +65,7 @@ public class CourseController {
         Map<String, String> map = new HashMap<>();
 
         // 수강신청
-        String message = courseRedisService.applyCourse(requestDto);
+        String message = courseService.applyCourse(requestDto);
 
         map.put("message",message);
 
